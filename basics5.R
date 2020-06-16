040b396bf53426a79e9949e7860d01aa37d34541
sneh-vora758 <snehvora2@gmail.com>
2020-06-16 15:34:24 +0530

    this is basics5.R

---
+++ basics5.R
@@ -0,0 +1,16 @@
+print("this is file in RStudio")
+print("i am sneh")
+
+
+x<-2
+print(x)
+as.character(x)
+as.complex(x)
+x=list(1,2,3,"x")
+print(x)
+x=matrix(1:6,ncol = 2,nrow = 3)
+print(x)
+x=1:3
+x<-1:3
+y<-4:6
+
