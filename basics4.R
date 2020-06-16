c6c4d6c67e453d01104fde34e662afa05de7701b
sneh-vora758 <snehvora2@gmail.com>
2020-06-16 15:32:44 +0530

    this is basics4.R

---
+++ basics4.R
@@ -0,0 +1,27 @@
+print("this is file in RStudio")
+print("i am sneh")
+
+
+x<-2
+print(x)
+as.character(x)
+as.complex(x)
+x=list(1,2,3,"x")
+print(x)
+x=matrix(1:6,ncol = 2,nrow = 3)
+print(x)
+x=1:3
+x<-1:3
+y<-4:6
+z<=c(x,y)
+z<-c(x,y)
+print(z)
+z<-c(x,y)
+print(x)
+x<-list(1,2,3,'a')
+print(x)
+x<-factor(c("yes","no","yes","yes"))
+x
+table(x)
+unclass(x)
+unclass(x)
